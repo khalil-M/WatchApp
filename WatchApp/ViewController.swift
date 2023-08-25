8082c79642d57a5f0bda687c71473751347063ef
Khalil Mhelheli <kmhelheli@mobelite.fr>
2023-08-25 08:33:15 +0100

    first commit

---
+++ WatchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchApp
+//
+//  Created by Khalil Mhelheli on 25/8/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
